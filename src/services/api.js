abc2ec5b356497c0340da8fbee378cd66e185c3a
mariobatalha <mariobatalha9@gmail.com>
2020-11-04 10:22:07 +0100

    change server

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,9 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:3333'
+    baseURL: process.env.DATABASE_URL
 })
 
 export default api;
+//http://localhost:3333
 //process.env.REACT_APP_API_URL
