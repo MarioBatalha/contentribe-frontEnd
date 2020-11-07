3df0434c406189992fb2c278dce9f9c7fff221f0
mariobatalha <mariobatalha9@gmail.com>
2020-11-07 18:39:00 +0100

    add create task

--- src/assets/pages/ProfileCompany/index.js
+++ src/assets/pages/ProfileCompany/index.js
@@ -1,55 +1,19 @@
 import React from 'react';
 import HeaderCompany from '../../components/HeaderCompany';
 import MenuCompany from '../../components/MenuCompany';
-import { Link } from 'react-router-dom';
-import { FaPlus, FaCoins, FaClock } from 'react-icons/fa';
-import api from '../../../services/api';
-import './style.css';
+import Task from '../Task';
 
 export default function ProfileCompany() {
-    const [request, setRequest] = useState([]);
-
-    const companyId = localStorage.getItem('companyId');
-
-    useEffect(() => {
-        api.get('/profile', { 
-          headers: {
-            authorization: companyId,
-          }
-        }).then(res => {
-          setRequest(res.data);
-        })
-      }, [companyId])
-
+   
     return (
-        <div className="profile-container">
+      <div className="profile-container">
           <HeaderCompany />
 
-         <div className="profile-container-body">
-         <MenuCompany />
+        <div className="profile-container-body">
+          <MenuCompany />
 
-            <div className="task-container">
-            <div className="task-card">
-                <div className="task-card-header">
-                <h2>Tradução do certificado de Habilitações Literárias</h2>
-                <h3>categoria - Tradução  </h3>
-                <h3> when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</h3>
-                <div className="task-detail">
-                    <label><FaCoins  color="#6920B0" /> Proposta: 60.000 AOA</label>
-                    <label><FaClock color="#6920B0" /> Duração: 15 dias úteis</label>
-                </div>
-                </div>
-            </div>  
-        
-            <div className="task-menu">
-            <Link to="/new/request" className="button-add">
-                <FaPlus size={20} color="#F29D35" />
-                Adicionar uma tarefa
-            </Link>
-            </div>  
+          <Task />
         </div>
-
-        </div>
-        </div>
+      </div>
     )
 }
--- src/assets/pages/Task/index.js
+++ src/assets/pages/Task/index.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import { FaPlus, FaCoins, FaClock } from 'react-icons/fa';
+import { FaCoins, FaClock } from 'react-icons/fa';
+import { FiPlus } from 'react-icons/fi';
 import api from '../../../services/api';
 import './style.css';
 
@@ -27,18 +28,15 @@ export default function Task() {
               <h3>categoria - Tradução  </h3>
               <h3> when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</h3>
               <div className="task-detail">
-                <label><FaCoins  color="#6920B0" /> Proposta: 60.000 AOA</label>
-                <label><FaClock color="#6920B0" /> Duração: 15 dias úteis</label>
+                <label><FaCoins  color="#F29D35" /> Proposta: 60.000 AOA</label>
+                <label><FaClock color="#F29D35" /> Duração: 15 dias úteis</label>
               </div>
             </div>
         </div>  
         
-        <div className="task-menu">
-          <Link to="/new/request" className="button-add">
-              <FaPlus size={20} color="#F29D35" />
-              Adicionar uma tarefa
-          </Link>
-        </div>  
+        <Link to="/new/request" className="create-task">
+            <FiPlus size={32} color="#FFF" />
+        </Link> 
       </div>
     )
 } 
