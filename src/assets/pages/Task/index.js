28ac01a87eb78b5a710e81344762f2200668c400
mariobatalha <mariobatalha9@gmail.com>
2020-11-16 17:46:19 +0100

    new request and task

--- src/assets/pages/Task/index.js
+++ src/assets/pages/Task/index.js
@@ -11,11 +11,7 @@ export default function Task() {
     const companyId = localStorage.getItem('companyId');
 
     useEffect(() => {
-        api.get('profile', {
-          headers: {
-            authorization: companyId,
-          }
-        }).then(res => {
+        api.get('profile').then(res => {
           setRequest(res.data);
         })
       }, [companyId])
